ad2fa5ede2e6647e259ea0737851786b805f60fb
Andrej Furtak <andfurto@gmail.com>
2011-08-11 22:45:00 +0200

    Pridanz modul: Posledne videne produkty.

---
+++ modules/blockviewed/sk.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{blockviewed}prestashop>blockviewed_43560641f91e63dc83682bc598892fa1'] = 'Prezerané produkty';
+$_MODULE['<{blockviewed}prestashop>blockviewed_49fa2426b7903b3d4c89e2c1874d9346'] = 'Viac o';
+$_MODULE['<{blockviewed}prestashop>blockviewed_859e85774d372c6084d62d02324a1cc3'] = 'Blok: Posledne videné produkty';
+$_MODULE['<{blockviewed}prestashop>blockviewed_3b2eba71c233156a12f94f26b3b188ab'] = 'Pridá blok s posledne videnými produktami';
+$_MODULE['<{blockviewed}prestashop>blockviewed_f4f70727dc34561dfde1a3c529b6205c'] = 'Nastavenie';
+$_MODULE['<{blockviewed}prestashop>blockviewed_e451e6943bb539d1bd804d2ede6474b5'] = 'Zobrazené produkty';
+$_MODULE['<{blockviewed}prestashop>blockviewed_8ce2b5b3ffa27a8a66220a49357a1582'] = 'Definuje počet zobrazených produktov v bloku';
+$_MODULE['<{blockviewed}prestashop>blockviewed_c9cc8cce247e49bae79f15173ce97354'] = 'Ulož';
+
+
+
+
+
